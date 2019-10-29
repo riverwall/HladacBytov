87ada360c646717330f68fa507033e70350b5e2e
Marián Hlavčo <marian_hlavco@tempest.sk>
2019-10-29 23:53:49 +0100

    runner do hlavneho projektu

---
+++ src/sk/hlavco/hladacBytovRun/Run.java
@@ -0,0 +1,27 @@
+package sk.hlavco.hladacBytovRun;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                try {
+                    Process proc = Runtime.getRuntime().exec("java -jar HladacBytov.jar");
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            }
+        };
+
+        timer.schedule(task, 1000,180000);
+    }
+}
